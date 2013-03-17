cc744d33fb65435c8d6537a5dc68ef1bc7f4cafb
hauss <philhaussler@gmail.com>
2013-03-17 13:57:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Philhaussler::Application.config.secret_token = 'dc6194b063bff7fe678e6819374ae91db827eb4dff714dde34d635277b4e684cc4bccf8b8e90c9a9216766f3af8ff57a74020655b773e398ff1f9692ee5e4be5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Philhaussler::Application.config.session_store :cookie_store, key: '_philhaussler_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Philhaussler::Application.config.session_store :active_record_store
